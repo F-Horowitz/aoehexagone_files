2a77cde9a8b6fcb0c765dc1bab566bcb33e1c5be
F-Horowitz <tennessix@gmail.com>
2016-03-20 00:52:01 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Funkumo Horowitz
+Page footer.php
+
+Pied de page. Contient mon copyright et les remerciements.
+*/
+?>		</div><footer>
+			<div class="footer_float_left"> <!-- Copyright -->
+			<p><?php echo $nom_site; ?> © Age of Empires Hexagone, 2013-2016. Tous les droits sur le site et son architecture HTML reviennent à son créateur.
+			Je remercie grandement les personnes qui m'ont soutenu dans la réalisation longue et ardue de ce projet. Merci grandement !<br/><br/>
+			Age of Empires appartient <span style="font-size: 0.7em;">(malheureusement!)</span> à <em>Microsoft Game Studios Inc.</em> et le mérite de son développement à <em>Ensemble Studios.</em>
+			 </div>
+			
+			<div class="footer_float_right"> <!-- Liens vers les forums disponibles -->
+				<h3>Accéder au forum..</h3>
+				<ul>
+					<li><a href="http://ageofempires-universe.fr/forums/forum_aoe/">Age of Empires</a></li>
+					<li><a href="http://ageofempires-universe.fr/forums/forum_aoe2/">Age of Empires II</a></li>
+					<li><a href="http://ageofempires-universe.fr/forums/forum_aoe3/">Age of Empires III</a></li>
+					<li><a href="http://www.jeuxvideo.com">jeuxvideo.com</a></li>
+				</ul></p>
+			</div>
+		</footer>
+	</body>
+</html>
